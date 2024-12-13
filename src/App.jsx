2dbef2659e61c5cae0a0c6bc09c75d78b53447c2
Maria <mariafacchin00@hotmail.com>
2024-12-13 14:51:12 +0100

    Fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import "./assets/styles/HomeDoors.css";
 
 function App() {
     return (
--- src/components/Doors.jsx
+++ src/components/Doors.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import "../assets/styles/HomeDoors.css";
 
 const Doors = () => {
     const handleClick = (day) => {

37c4b092d80c51365ba7fe9c83f9e6a56cfe177d
Maria <mariafacchin00@hotmail.com>
2024-12-13 14:29:27 +0100

    Add basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
     return (
         <>
-        Testing
+        <Home />
         </>
     )
 }
